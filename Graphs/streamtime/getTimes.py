db50a58e20a31c6938576a70c9c5bb468bf188a1
Jones <jonesn2@wit.edu>
2019-04-17 18:22:47 -0400

    nathan files

--- Graphs/streamtime/getTimes.py
+++ Graphs/streamtime/getTimes.py
@@ -1,22 +1,46 @@
 #### get hours streamed, daily broadcast time, active days per week
 import pandas as pd 
-from mpl_toolkits import mplot3d
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-data = pd.read_csv('twitch.csv')
-
-# print(data.columns)
+data = pd.read_csv('../../ScrapedTwitch/twitch.csv')
 
 totHrs = data.Hours_Streamed
+totDays = data.Days_Active
 dailyHrs = data.Daily_Broadcast_Time
 dpw = data.Active_Days_per_Week
+followers = data.Followers_to_Date
 
-# print(totHrs[:5])
-# print(dailyHrs[:5])
-# print(dpw[:5])
-# print(totHrs[0])
+plt.scatter(dpw,followers)
+plt.ticklabel_format(style='plain',axis='y')
+plt.title("Effect of Active Days per Week")
+plt.xlabel("Active Days")
+plt.ylabel("Followers")
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-fig.show()
+plt.scatter(totHrs,followers)
+plt.ticklabel_format(style='plain',axis='y')
+plt.title("Effect of Total Hours Streamed")
+plt.xlabel("Total Hours")
+plt.ylabel("Followers")
+
+plt.scatter(dailyHrs,followers)
+plt.ticklabel_format(style='plain',axis='y')
+plt.title("Effect of Hours Active per Day")
+plt.xlabel("Daily Hours")
+plt.ylabel("Followers")
+
+plt.scatter(totDays,followers)
+plt.ticklabel_format(style='plain',axis='y')
+plt.title("Effect of Total Days Streamed")
+plt.xlabel("Total Days")
+plt.ylabel("Followers")
+
+plt.scatter(dpw,totHrs,s=followers*.00001,c=(0,0,1))
+plt.title("Active Days and Total Hours vs Followers")
+plt.xlabel("Active Days")
+plt.ylabel("Total Hours")
+
+plt.scatter(dpw,dailyHrs,s=followers*.00001,c=(0,0,1))
+plt.title("Active Days and Daily Hours vs Followers")
+plt.xlabel("Active Days")
+plt.ylabel("Daily Hours")
